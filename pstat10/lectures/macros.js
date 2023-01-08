2c262b992b08d1c11f9a4d72cd9f89dbf474eb5a
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-01-08 23:51:13 +0000

    update gemfile for local docker testing [ci skip]

--- pstat10/lectures/macros.js
+++ pstat10/lectures/macros.js
@@ -1,4 +1,1 @@
-remark.macros.scale = function(w) {
-  var url = this;
-  return '<img src="' + url + '" style="width: ' + w + '" />';
-};
+remark.macros.scale=function(r){return'<img src="'+this+'" style="width: '+r+'" />'};
